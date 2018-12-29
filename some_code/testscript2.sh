9df2c9d093a7fbee6a35e72359a8255f6e2e05e6
epocolis <leotisbuchanan@tutanota.com>
2018-12-29 12:13:52 -0500

    ororo

---
+++ some_code/testscript2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "this script is from github"
+NUMBER=$(($NUMBERONE + $NUMBERTWO))
+echo "$NUMBER"
+echo "this was run automagically after being updated again"
+echo "this was run automagically after being updated again"
